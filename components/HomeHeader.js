6fa1bbe62d7a8c9ddb3205ff677ae9d11b0926f4
jonathanpenaloa <jona_pena_91@outlook.com>
2023-02-13 16:51:32 -0800

    added input to the header and styled

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -2,9 +2,9 @@ import { View, Text, Image, TextInput} from 'react-native'
 
 import { COLORS, FONTS, SIZES, assets } from '../constants'
 
-import React from 'react'
+import React, { version } from 'react'
 
-const HomeHeader = () => {
+const HomeHeader = ({ onSearch }) => {
   return (
     <View style={{
       backgroundColor: COLORS.primary,
@@ -28,6 +28,39 @@ const HomeHeader = () => {
           source={assets.badge}
           resizeMethod="contain"
           style={{ position: "absolute",  width: 15, height: 15, bottom: 0, right: 0 }}
+          />
+        </View>
+      </View>
+      <View style={{ marginVertical: SIZES.font  }} >
+        <Text style={{ fontFamily: FONTS.regular, fontSize: SIZES.small, 
+        color: COLORS.white }} >
+          Hello, Jon 👋 
+        </Text>
+        <Text style={{ fontFamily: FONTS.bold, fontSize: SIZES.large, 
+        color: COLORS.white, marginTop: SIZES.base / 2 }} >
+          Find some art
+        </Text>
+      </View>
+
+      <View style={{ marginTop: SIZES.font }} >
+      <View style={{
+          width: "100%",
+          borderRadius: SIZES.font,
+          backgroundColor: COLORS.gray,
+          flexDirection: "row",
+          alignItems: "center",
+          paddingHorizontal: SIZES.font,
+          paddingVertical: SIZES.small - 2,
+      }}>
+          <Image 
+          source={assets.search}
+          resizeMode="contain"
+          style={{ width: 20, height: 20, marginRight: SIZES.base }}
+          />
+          <TextInput
+            placeholder="Search NFTs"
+            style={{ flex: 1 }}
+            onChangeText={onSearch}
           />
         </View>
       </View>
